76607fde60f1cd0ade4dc4aa3bfd59a854b2c34e
duke <webcreation33@yahoo.fr>
2022-04-27 11:50:09 +0200

    AV07

--- Com.Core.Dotsafe.UI/Dtos/ContributionDto.cs
+++ Com.Core.Dotsafe.UI/Dtos/ContributionDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Com.Core.Dotsafe.UI.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Com.Core.Dotsafe.UI.Dtos
 {
     public class ContributionDto
     {
         public int Id { get; set; }
+
+        [Required]
         public string name { get; set; }
         public string test { get; set; }
     }
--- libs/Com.Core.Dotsafe.Domain/Contribution.cs
+++ libs/Com.Core.Dotsafe.Domain/Contribution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -10,6 +11,7 @@ namespace Com.Core.Dotsafe.Domain
     public class Contribution : BaseEntity
     {
         public int Id { get; set; }
+
         public string name { get; set; }
 
         //[JsonIgnore]
